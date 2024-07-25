08c6d5b0768498e610faf446d16595443a1d71c3
abgaonkar <abgaonkar@synamedia.com>
2024-07-25 16:53:15 +0530

    ADD: 912

--- Leet-Code/912-sort-an-array.ts
+++ Leet-Code/912-sort-an-array.ts
@@ -15,4 +15,26 @@ Output: [0,0,1,1,2,5]
 Explanation: Note that the values of nums are not necessairly unique.
 
  */
-function sortArray(nums: number[]): number[] {}
+function sortArray(nums: number[]): number[] {
+  function merge(leftArr: number[], rightArr: number[]): number[] {
+    let sortedArr = [];
+    while (leftArr.length && rightArr.length) {
+      if (leftArr[0] < rightArr[0]) {
+        sortedArr.push(leftArr.shift());
+      } else {
+        sortedArr.push(rightArr.shift());
+      }
+    }
+    return [...sortedArr, ...leftArr, ...rightArr];
+  }
+  function mergeSort(arr: number[]): number[] {
+    if (arr.length <= 1) return arr;
+    let mid = Math.floor(arr.length / 2);
+    let leftArr = mergeSort(arr.slice(0, mid));
+    let rightArr = mergeSort(arr.slice(mid));
+
+    return merge(leftArr, rightArr);
+  }
+  return mergeSort(nums);
+}
+console.log(sortArray([5, 1, 1, 2, 0, 0]));
